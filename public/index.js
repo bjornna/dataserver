ea1aa50219903b8019ff3afde2959f38ade507c7
Bjørn Næss <bna@dips.no>
2018-06-16 15:02:37 +0200

    Charts for all vitalia

--- public/index.js
+++ public/index.js
@@ -3,52 +3,45 @@
     loadVitalSigns();
     console.log("Initialized - starting the loop");
     loopInterval = 2000;
+    var chartSeriesLength = 40;
     loop(loopInterval);
+    var bpDataSystolic = [];
+    var bpDataDiastolic = [];
 
+    var bpChart = new Chartist.Line('#bp-chart', 
+    {
+            labels: [],
+            series: [
+                bpDataSystolic, bpDataDiastolic
+            ]
+        }, getOptions([50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 150])   );
     var spo2Data = [];
     var spo2Chart = new Chartist.Line('#spo2-chart', {
+            labels: [],
+            series: [
+                pulseData
+            ]
+        },
+        getOptions([60, 70, 80, 90, 100])
+    );
+
+    var tempData = [];
+    var tempChart = new Chartist.Line('#temp-chart', {
         labels: [],
         series: [
-            pulseData
+            tempData
         ]
-    },
-    getOptions([60, 70, 80, 90, 95, 100])
-);
-
+    }, getOptions([35, 36, 37, 38, 39, 40, 41, 42, 43]));
 
     var pulseData = [];
-
     var pulseChart = new Chartist.Line('#pulse-chart', {
         labels: [],
         series: [
             pulseData
         ]
-    }, getOptions([60, 70, 80, 90, 100,110]));
+    }, getOptions([60, 70, 80, 90, 100, 110]));
 
-function getOptions(yTicks){
-    
-// We are setting a few options for our chart and override the defaults
-var options = {
-    // Don't draw the line chart points
-    showPoint: false,
-    // Disable line smoothing
-    lineSmooth: true,
-    // X-Axis specific configuration
-    axisX: {
-      // We can disable the grid for this axis
-      showGrid: true,
-      // and also don't show the label
-      showLabel: false
-    },
-    // Y-Axis specific configuration
-    axisY: {
-        type: Chartist.FixedScaleAxis,
-        ticks: yTicks,
-        low: yTicks[0]
-    }
-  };
-  return options;
-}
+   
 
     function loop(ms) {
         setTimeout(function () {
@@ -62,7 +55,7 @@ var options = {
             var d = data.magnitude;
 
             pulseData.push(d);
-            if (pulseData.length > 20) {
+            if (pulseData.length > chartSeriesLength) {
                 pulseData.shift();
             }
 
@@ -76,15 +69,30 @@ var options = {
             }
             document.getElementById("pulse").textContent = d;
             pulseChart.update(data);
-            
+
         });
         loadTemp(function (data) {
-            document.getElementById("temp").textContent = data.magnitude;
+            var temp = data.magnitude;
+            document.getElementById("temp").textContent = temp;
+
+            tempData.push(temp);
+            if (tempData.length > chartSeriesLength) {
+                tempData.splice();
+            }
+            var labels = [];
+            for (let index = 0; index < tempData.length; index++) {
+                labels.push(index);
+            }
+            var data = {
+                labels: labels,
+                series: [tempData]
+            }
+            tempChart.update(data);
         });
         loadSpO2(function (data) {
             document.getElementById("spo2").textContent = data.magnitude;
             spo2Data.push(data.magnitude);
-            if (spo2Data.length > 30) {
+            if (spo2Data.length > chartSeriesLength) {
                 spo2Data.shift();
             }
             var labels = [];
@@ -101,7 +109,51 @@ var options = {
         loadBloodPressure(function (data) {
             let s = data.systolic;
             let d = data.diastolic;
+            bpDataDiastolic.push(d);
+            bpDataSystolic.push(s);
+            if (bpDataSystolic.length > chartSeriesLength) {
+                bpDataDiastolic.splice();
+                bpDataSystolic.splice();
+
+            }
+            var labels = [];
+            for (let index = 0; index < bpDataDiastolic.length; index++) {
+                labels.push(index);
+
+            }
+            var data = {
+                labels: labels,
+                series: [bpDataSystolic, bpDataDiastolic]
+            }
+
             document.getElementById("bp").textContent = s + "/" + d;
+            bpChart.update(data);
         });
     }
+
+    function getOptions(yTicks) {
+
+        // Shared options for all the charts 
+        var options = {
+            // Don't draw the line chart points
+            showPoint: false,
+            // Disable line smoothing
+            lineSmooth: true,
+            // X-Axis specific configuration
+            axisX: {
+                // We can disable the grid for this axis
+                showGrid: true,
+                // and also don't show the label
+                showLabel: false
+            },
+            // Y-Axis specific configuration
+            axisY: {
+                type: Chartist.FixedScaleAxis,
+                ticks: yTicks,
+                low: yTicks[0]
+            }
+        };
+        return options;
+    }
+
 })();
